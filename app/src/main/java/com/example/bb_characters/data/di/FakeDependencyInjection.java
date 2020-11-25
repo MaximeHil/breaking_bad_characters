9afc6c9cc3538d41fb9f47255c3388397438f125
unknown <maxime.hilberer.etu@univ-lille.fr>
2020-11-25 14:07:30 +0100

    Instance retrofit dans le fakeDI

--- app/src/main/java/com/example/bb_characters/data/di/FakeDependencyInjection.java
+++ app/src/main/java/com/example/bb_characters/data/di/FakeDependencyInjection.java
@@ -1,9 +1,45 @@
 package com.example.bb_characters.data.di;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import com.google.gson.Gson;
+
 /**
  * Cette classe permet de simuler l'utilisation d'un système d'injection de dépendances
  * Les repository sont initialisés une seule fois au lancement de l'application
  */
 public class FakeDependencyInjection {
 
+    private static Retrofit retrofit;
+    private static Gson gson;
+
+    //Création d'une instance pour utiliser l'interface Retrofit
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://www.breakingbadapi.com/api/")
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(getGson()))
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static Gson getGson() {
+        if (gson == null) {
+            gson = new Gson();
+        }
+        return gson;
+    }
 }
